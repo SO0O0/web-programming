e280e0ee3a714c37bc92d28483de3612bc07fe11
SO000000 <hirawatasou@gmail.com>
2022-09-26 19:04:21 +0900

    :sparkles:第2回課題3

---
+++ task2/3.php
@@ -0,0 +1,11 @@
+<?php echo '
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+        <p> Hello PHP world! </p>
+    </body>
+</html>';
