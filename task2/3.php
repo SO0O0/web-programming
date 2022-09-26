348ef976203015f15d90223105751d0ba12fd91d
SO000000 <hirawatasou@gmail.com>
2022-09-26 19:34:52 +0900

    :recycle:第2回課題3

--- task2/3.php
+++ task2/3.php
@@ -1,11 +1,12 @@
-<?php echo '
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>ページタイトル</title>
-    </head>
-    <body>
-        <p> Hello PHP world! </p>
-    </body>
-</html>';
+<?php echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8" />
+            <title>ページタイトル</title>
+        </head>
+        <body>
+            <p> Hello PHP world! </p>
+        </body>
+    </html>'
+EOT;
