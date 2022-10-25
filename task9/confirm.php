69346f1767ae1de1f634cff00b121fb694a3c50c
SO000000 <hirawatasou@gmail.com>
2022-10-25 13:06:30 +0900

    :construction:9-1

---
+++ task9/confirm.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>なんちゃって掲示板システム</title>
+</head>
+
+<body>
+    <?php
+    // データベースの情報
+    $hostname = '127.0.0.1';
+    $username = 'root';
+    $password = 'dbpass';
+    $dbname = 'board_db';
+    $tablename = 'board_table';
+
+    // データベースに接続
+    $link = mysqli_connect($hostname, $username, $password);
+    // 接続に失敗した場合
+    if (!$link) {
+        exit('データベースに接続できませんでした。');
+    }
+
+    // データベースがなければ作成
+    $result = mysqli_query($link, "CREATE DATABASE IF NOT EXISTS $dbname CHARACTER SET utf8");
+    // 作成に失敗した場合
+    if (!$result) {
+        exit('データベースの作成に失敗しました。');
+    }
+
+    // データベースを選択
+    $result = mysqli_query($link, "use $dbname");
+    // 選択に失敗した場合
+    if (!$result) {
+        exit('データベースの選択に失敗しました。');
+    }
+
+    // テーブルがなければ作成
+    $result = mysqli_query($link, "CREATE TABLE IF NOT EXISTS $tablename (
+        time TIMESTAMP NOT NULL,
+        nominal VARCHAR(20) BINARY,
+        price INT NOT NULL,
+        flow INT NOT NULL,
+        PRIMARY KEY(time))
+    )");
+    // 作成に失敗した場合
+    if (!$result) {
+        exit('テーブルの作成に失敗しました。');
+    }
+    ?>
+
+    <h1>なんちゃって掲示板システム</h1>
+    <div class="container">
+        <p>以下の内容でよろしいですか？</p>
+        <div class="confirm-contents">
+            <?php
+            $article = $_POST['article'];
+            echo  $article;
+            ?>
+        </div>
+        <button onclick="location.href='./index.php'">投稿</button>
+    </div>
+</body>
+
+</html>
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 80vh;
+    }
+
+    .container {
+        width: 80%;
+        max-width: 1000px;
+        height: 80vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 75px;
+    }
+
+    .confirm-contents {
+        max-width: 100%;
+        width: 600px;
+        height: 500px;
+        border: 3px solid #404040;
+        padding: 10px;
+        overflow: scroll;
+    }
+
+    p {
+        font-size: 25px;
+        font-weight: bold;
+    }
+
+    button {
+        font-size: 1.0rem;
+        font-weight: bold;
+        line-height: 1.0;
+        position: relative;
+        display: inline-block;
+        padding: 0.5rem 2.0rem;
+        cursor: pointer;
+        border: 0.1rem solid #000;
+        transition: all 0.3s;
+        text-align: center;
+        vertical-align: middle;
+        text-decoration: none;
+        letter-spacing: 0.1em;
+        color: #212529;
+        border-radius: 0.5rem;
+        margin-top: 1rem;
+    }
+
+    button:hover {
+        background-color: #000;
+        color: #FFFFFF;
+    }
+</style>
